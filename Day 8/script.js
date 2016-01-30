88863bf0ddaa7fec98ac1c028b8a2fa2fcb1668d
Pablo Alonso Lorente <pablochux@me.com>
2016-01-30 20:36:45 +0100

    Fixes
    Changed variables for an object

--- Day 8/script.js
+++ Day 8/script.js
@@ -1,18 +1,17 @@
 $(document).ready(function(){
-	var selectedColour, elementClicked;
+	var element = {};
 	$('.colour').click(function(){
-		// Controller for 
-		if (elementClicked != undefined) {
-			$(elementClicked).removeClass('clicked');
-			console.log('Element clicked != undefined');
+		// Controller for clicked available colors
+		if (element.data != undefined) {
+			$(element.data).removeClass('clicked');
 		};
 		// Assign the variables
-		elementClicked = $(this);
-		selectedColour = $(this).css('background-color');
+		element.data = $(this);
+		element.backgroundColour = $(this).css('background-color');
 		$(this).addClass('clicked'); // Add the clicked effect
 		// When a .clickable element is clicked, it's background color is changed 
 		$('section .clickable').click(function(){
-			$(this).css('background-color', selectedColour);
+			$(this).css('background-color', element.backgroundColour);
 		});
 	});
 });
