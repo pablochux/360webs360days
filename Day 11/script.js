cf2f8a8c56ecd3a14393fb2699f50dbc4ad7ab20
Pablo Alonso Lorente <pablochux@me.com>
2016-02-02 10:25:26 +0100

    Day 11 finished 👊🏻 ✔️

--- Day 11/script.js
+++ Day 11/script.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-	 
+	var i = 1;
+	var date = new Date();
+	var phrases = [
+		"Sunday Funday!",
+	    "Back to the grind!",
+	    "Oh. It's Tuesday.",
+	    "Boom! Wednesday!",
+	    "Thirsty Thursday, yo!",
+	    "It's Friday! We should be kickin' it!",
+	    "Paaarrrtttyyy!"
+	 ];
+	// Select the day
+	$('#'+date.getDay()).addClass('today');
+	// Add the phrase
+	$('.phrase h1').text(phrases[date.getDay()]);
+	// Displays another phrase (NW)
+	$('.btn').click(function(){
+		if (date.getDay() + i < 7) { 
+			// Changes phrase
+			$('.phrase h1').text(phrases[date.getDay() + i++]);
+		} else{
+			// Diplay Week is Over message
+			$(this).text('Sorry. Week is Over.').removeClass('btn-effect').addClass('disabled');
+		}
+	});
 });
