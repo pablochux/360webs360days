5d2839bbf95849cbf5b48b72478b80e023a298d5
Pablo Alonso Lorente <pablochux@me.com>
2016-02-03 14:46:29 +0100

    Day 13 finished 👊🏻 ✔️

--- Day 13/script.js
+++ Day 13/script.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
-	 $('.horizontal-line').mouseover(function(){
-	 	console.log('Mouseover')
-	 	$(this).css('background-color', getRandomColor());
-	 })
+	$('.horizontal-line').mouseover(function(){
+		console.log('Mouseover')
+		$(this).css('background-color', getRandomColor());
+	});
+	$('button').click(function(){
+		$('.horizontal-line').each(function(){
+			$(this).css('background-color', getRandomColor());
+		});
+	});
+	// for (var i = 0; i <= 70; i++) {
+	// 	console.log('Inside Test');
+	// 	setTimeout(function(){
+	// 		$('.horizontal-line').eq(i).css('background-color', getRandomColor());
+	// 	}, 100);
+	// };
 });
 
 function getRandomColor() {
