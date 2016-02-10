0311349b0c224aee086401a6026a074c0123dc82
Pablo Alonso Lorente <pablochux@me.com>
2016-02-10 10:19:16 +0100

    Day 19 finished 👊🏻 ✔️

--- Day 19/script.js
+++ Day 19/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$('.btn').click(function(){
-		$(this).fadeOut(100);
-		var button = '.btn-' + Math.round((Math.random() * 4));
+		$(this).hide(0);
+		var button = '.btn-' + Math.floor((Math.random() * 4) + 1);
 		console.log(button);
 		var top = getRandomPosition() + 'px';
 		var left = getRandomPosition() + 'px';
@@ -9,9 +9,11 @@ $(document).ready(function(){
 		console.log(left);
 		$(button).css('top', top);
 		$(button).css('left', left);
-		$(button).css('display', 'block');
+		setTimeout(function(){
+			$(button).show(1500);
+		}, 500);
 	});
 });
 function getRandomPosition() {
-	return Math.round(Math.random() * 200);
+	return Math.round(Math.random() * 300);
 }
