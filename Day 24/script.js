68a4b70003282e282806d2b46732ecd4fcd2233f
Pablo Alonso Lorente <pablochux@me.com>
2016-02-16 23:33:54 +0100

    Added logic for entering and displaying new habits (NW)

--- Day 24/script.js
+++ Day 24/script.js
@@ -1,3 +1,7 @@
-$(document).ready(function(){
-	 
+$(document).ready(function() {
+	$('button[name="submit"]').click(function(){
+		$('h2').fadeOut(0);
+		$('h2').text($('input').val());
+		$('h2').fadeIn(500);
+	});
 });
